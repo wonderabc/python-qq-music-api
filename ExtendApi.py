8905dc275d9f01514f321b894937ba5fb03315a8
zhangyilin20 <wonderabc_ustb@outlook.com>
2021-03-15 15:45:03 +0800

    增加歌曲搜索API

--- ExtendApi.py
+++ ExtendApi.py
@@ -1,5 +1,7 @@
 # 实现一些扩展功能
-# 1. 返回接口第一页（maximum 100首）收藏数前10的歌曲信息
+# 1. 通过歌手姓名（与QQ音乐显示的完全一致）返回接口第一页（maximum 100首）收藏数前k的歌曲信息
+# 2. 通过歌名获得最相关的十首歌曲的收藏信息
+import json
 from operator import itemgetter
 from Api import Api
 
@@ -18,10 +20,8 @@ class ExtendApi:
         singerInfo = "；".join(singerList)
         return singerInfo
 
-
-
     @staticmethod
-    def GetTopkFavbyName(k, singername):  # 返回接口第一页（maximum 100首）收藏数前k的歌曲信息
+    def GetTopkFavbyName(k, singername):  # 通过歌手姓名（与QQ音乐显示的完全一致）返回接口第一页（maximum 100首）收藏数前k的歌曲信息
         """
         :param k: 返回收藏数前k的歌曲信息
         :param singername: 歌手名
@@ -41,5 +41,22 @@ class ExtendApi:
         songInfoList = sorted(songInfoList, key=itemgetter('FavNum'), reverse=True)  # reverse=True表示降序
         songInfoList = songInfoList[:k]
         return songInfoList
+
+    @staticmethod
+    def GetRelasongFavInfo(songName):  # 通过歌名获得最相关的十首歌曲的收藏信息
+        result = Api.keywordSearch(songName, type='lyric')
+        songList = list(json.loads(result)['data']['lyric']['list'])
+        songInfoList = []
+        for song in songList:
+            songID = str(song['id'])
+            songName = song['title']
+            singerInfo = ExtendApi.GetSingerList(song['singer'])
+            FavNum = Api.getMusicFavNum(songID)[songID]
+            songInfo = {'songName': songName, 'singerInfo': singerInfo, 'FavNum': int(FavNum)}
+            songInfoList.append(songInfo)
+        return songInfoList
+
+
+
 
 
